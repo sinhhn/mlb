6d99e9dad3cb0069a8f207cc54828637d854029e
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-06-15 14:37:32 +0900

    test

---
+++ wc.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as matplot
+import seaborn as sns
+import pandas as pd
+df = pd.read_csv('../results.csv', index_col=None)
+print df.head()
